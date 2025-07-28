5afb04891a8b51b4433a8167e1ccfdb2f26613da
Yariv Gilad <yariv.gilad@gmail.com>
2025-07-28 12:27:51 +0300

    updated packages versions

--- src/view/Vacation.tsx
+++ src/view/Vacation.tsx
@@ -3,21 +3,28 @@ interface Props {
 }
 
 export function Vacation({ available }: Props) {
-  if (available === true) {
-    return <h1> Going on vacation </h1>;
-  } else {
-    return <h1> Too busy for Vacation </h1>;
-  }
+  // if (available === true) {
+  //   return <h1> Going on vacation </h1>;
+  // } else {
+  //   return <h1> Too busy for Vacation </h1>;
+  // }
+
+  const msg =
+    available === true ? "Going on vacation" : "Too busy for Vacation";
+
+  return (
+    <h1>{msg}</h1>
+  );
 
 }
-  // return (
-  //   <h1>
-  //     {available === true ? "Going on vacation" : "Too busy for Vacation"}
-  //     {available === true && "Going on vacation" }
-  //   </h1>
-  // );
+// return (
+//   <h1>
+//     {available === true ? "Going on vacation" : "Too busy for Vacation"}
+//     {available === true && "Going on vacation" }
+//   </h1>
+// );
 
-    // const msg =
-    //   available === true ? "Going on vacation" : "Too busy for Vacation";
+// const msg =
+//   available === true ? "Going on vacation" : "Too busy for Vacation";
 
-    // return <h1>{msg}</h1>;
+// return <h1>{msg}</h1>;
